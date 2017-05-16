697e1c0bb29cf99cac66093ffd5ff618ec30cf07
maciejgawlowski <maciejgawlowski@tlen.pl>
2017-05-16 18:50:19 +0200

    Course

---
+++ springboot-jpa/src/main/java/pl/gawlowski/maciej/service/CourseRepository.java
@@ -0,0 +1,13 @@
+package pl.gawlowski.maciej.service;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import pl.gawlowski.maciej.model.Course;
+
+public interface CourseRepository extends CrudRepository<Course, String> { //String to typ primary key (id)
+
+	//Spring Data JPA sam implementuje tą metodę! nie muszę jej sam pisać, tylko deklarować ją w interfejsie!!!
+	public List<Course> findByTopicId(String topicId); 
+}
---
+++ springboot-jpa/src/main/java/pl/gawlowski/maciej/service/CourseService.java
@@ -0,0 +1,40 @@
+package pl.gawlowski.maciej.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pl.gawlowski.maciej.model.Course;
+import pl.gawlowski.maciej.model.Topic;
+
+@Service
+public class CourseService {
+
+	@Autowired
+	private CourseRepository courseRepository;
+	
+	public List<Course> getAllCourses(String topicId) {
+		List<Course> courses = new ArrayList<>();
+		courseRepository.findByTopicId(topicId).forEach(courses::add);
+		return courses;
+	}
+
+	public Course getCourse(String id) {
+		return courseRepository.findOne(id);
+	}
+
+	public void addCourse(Course course) {
+		courseRepository.save(course);
+	}
+
+	public void updateCourse(Course course) {
+		courseRepository.save(course); //db wie że jest takie id jakie zapisujemy i je uaktualni, a jak nie będzie to utworzy nowe
+	}
+
+	public void deleteCourse(String id) {
+		courseRepository.delete(id);
+	}
+
+}
